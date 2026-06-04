1c39af4bf9b0d8cf44515c6a181d5f079e9d8ba8
Amirhosseinmirzaei <amir13881188@gmail.com>
2026-06-04 14:46:21 +0330

    printing links of pictures of fararu.com

--- 1.py
+++ 1.py
@@ -2,16 +2,19 @@ from bs4 import BeautifulSoup
 import requests
 w=requests.get("https://fararu.com")
 soup=BeautifulSoup(w.content)
-divs=soup.find_all("div",{"class":"same_box same_header"})
+divs=soup.find_all("div", {"class":"same_box same_header"})
 div=divs[1]
 links=div.find_all("a")
+seen_img=[]
 for link in links:
-    p=("https://fararu.com"+link["href"]+"\n")
-    #print(p)
+    p="https://fararu.com"+link["href"]
     q=requests.get(p)
     soup=BeautifulSoup(q.content)
     divs=soup.find_all("div",{"class":"primary_files"})
     for div in divs:
-        links=div.find_all("img")
-        for link in links:
-            print(link["src"],"\n")
+        imgs=div.find_all("img")
+        for img in imgs:
+            src=img.get("src")
+            if src not in seen_img:
+                seen_img.append(src)
+                print(src)
